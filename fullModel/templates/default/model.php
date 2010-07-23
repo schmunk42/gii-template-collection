b8e1da7637dca0225775002a0c75253cc201cd8b
robregonm <robregonm@fa601203-8fba-3265-82bf-2153ca8df311>
2010-07-23 21:05:26 +0000

    Added comments in generated model file for better autocompletion.
    git-svn-id: https://gii-template-collection.googlecode.com/svn/trunk/gtc@11 fa601203-8fba-3265-82bf-2153ca8df311

--- fullModel/templates/default/model.php
+++ fullModel/templates/default/model.php
@@ -14,35 +14,61 @@
 ?>
 <?php echo "<?php\n"; ?>
 
+/**
+ * This is the model class for table "<?php echo $tableName; ?>".
+ *
+ * The followings are the available columns in table '<?php echo $tableName; ?>':
+<?php foreach($columns as $column): ?>
+ * @property <?php echo $column->type.' $'.$column->name."\n"; ?>
+<?php endforeach; ?>
+ */
 class <?php echo $modelClass; ?> extends <?php echo $this->baseClass."\n"; ?>
 {
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return <?php echo $modelClass; ?> the static model class
+	 */
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
 	}
 
+	/**
+	 * @return string the associated database table name
+	 */
 	public function tableName()
 	{
 		return '<?php echo $tableName; ?>';
 	}
 
+	/**
+	 * @return array validation rules for model attributes.
+	 */
 	public function rules()
 	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
 		return array(
 <?php foreach($rules as $rule): ?>
 			<?php echo $rule.",\n"; ?>
 <?php endforeach; ?>
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
 			array('<?php echo implode(', ', array_keys($columns)); ?>', 'safe', 'on'=>'search'),
 		);
 	}
 
+	/**
+	 * @return array relational rules.
+	 */
 	public function relations()
 	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
 		return array(
-<?php foreach($relations as $name=>$relation) {
-			echo "\t\t\t'$name' => $relation,\n"; 
-}
-?>
+<?php foreach($relations as $name=>$relation): ?>
+			<?php echo "\t\t\t'$name' => $relation,\n"; ?>
+<?php endforeach; ?>
 		);
 	}
 
@@ -53,6 +79,9 @@ class <?php echo $modelClass; ?> extends <?php echo $this->baseClass."\n"; ?>
 				);
 	}
 
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
 	public function attributeLabels()
 	{
 		return array(
@@ -62,6 +91,10 @@ class <?php echo $modelClass; ?> extends <?php echo $this->baseClass."\n"; ?>
 		);
 	}
 
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
 	public function search()
 	{
 		$criteria=new CDbCriteria;
