41c059b07756fdd8a6e29f19f361b643e70f288a
robregonm <robregonm@fa601203-8fba-3265-82bf-2153ca8df311>
2010-07-27 19:36:15 +0000

    Comments in model template removed.
    git-svn-id: https://gii-template-collection.googlecode.com/svn/trunk/gtc@13 fa601203-8fba-3265-82bf-2153ca8df311

--- fullModel/templates/default/model.php
+++ fullModel/templates/default/model.php
@@ -24,50 +24,33 @@
  */
 class <?php echo $modelClass; ?> extends <?php echo $this->baseClass."\n"; ?>
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return <?php echo $modelClass; ?> the static model class
-	 */
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
 	}
 
-	/**
-	 * @return string the associated database table name
-	 */
 	public function tableName()
 	{
 		return '<?php echo $tableName; ?>';
 	}
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return array(
 <?php foreach($rules as $rule): ?>
 			<?php echo $rule.",\n"; ?>
 <?php endforeach; ?>
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
 			array('<?php echo implode(', ', array_keys($columns)); ?>', 'safe', 'on'=>'search'),
 		);
 	}
 
-	/**
-	 * @return array relational rules.
-	 */
 	public function relations()
 	{
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
 <?php foreach($relations as $name=>$relation): ?>
-			<?php echo "\t\t\t'$name' => $relation,\n"; ?>
+			<?php echo "'$name' => $relation,\n"; ?>
 <?php endforeach; ?>
 		);
 	}
@@ -79,9 +62,6 @@ class <?php echo $modelClass; ?> extends <?php echo $this->baseClass."\n"; ?>
 				);
 	}
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
 	public function attributeLabels()
 	{
 		return array(
@@ -91,10 +71,6 @@ class <?php echo $modelClass; ?> extends <?php echo $this->baseClass."\n"; ?>
 		);
 	}
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
 	public function search()
 	{
 		$criteria=new CDbCriteria;
