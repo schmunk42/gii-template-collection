b329b09cc1c6bab3cf06c10a8689c16e401dca3e
robregonm <robregonm@fa601203-8fba-3265-82bf-2153ca8df311>
2010-08-04 15:25:50 +0000

    Added ComboBoxes to Relational Fields in Advanced search form.
    git-svn-id: https://gii-template-collection.googlecode.com/svn/trunk/gtc@54 fa601203-8fba-3265-82bf-2153ca8df311

--- fullCrud/templates/default/_search.php
+++ fullCrud/templates/default/_search.php
@@ -13,10 +13,24 @@
 ?>
         <div class="row">
                 <?php echo "<?php echo \$form->label(\$model,'{$column->name}'); ?>\n"; ?>
+<?php if(!$column->isForeignKey):?>
                 <?php echo "<?php ".$this->generateActiveField($this->modelClass,$column)."; ?>\n"; ?>
+<?php else: ?>
+                <?php echo "<?php echo ".$this->generateValueField($this->modelClass,$column,'search')."; ?>\n"; ?>
+<?php endif; ?>
         </div>
-
 <?php endforeach; ?>
+<?php /*foreach($this->getRelations() as $key => $relation)
+{
+	if($relation[0] == 'CBelongsToRelation'
+			or $relation[0] == 'CHasOneRelation'
+			or $relation[0] == 'CManyManyRelation')
+	{
+		printf('<label for="%s">%s</label>', $relation[1], $relation[1]);
+		echo "<?php echo CHtml::listData({$relation[1]}::model()->findAll(), 'id', 'nombre'". $this->generateRelation($this->modelClass, $key, $relation)."; ?>\n";
+	}
+}*/
+?>
         <div class="row buttons">
                 <?php echo "<?php echo CHtml::submitButton(Yii::t('app', 'Search')); ?>\n"; ?>
         </div>
