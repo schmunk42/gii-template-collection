f37d3053a9c1b13ba847742bcbda193316300ad7
robregonm <robregonm@fa601203-8fba-3265-82bf-2153ca8df311>
2010-08-04 15:30:55 +0000

    Removed unnecessary comment in search template.
    git-svn-id: https://gii-template-collection.googlecode.com/svn/trunk/gtc@55 fa601203-8fba-3265-82bf-2153ca8df311

--- fullCrud/templates/default/_search.php
+++ fullCrud/templates/default/_search.php
@@ -19,18 +19,8 @@
                 <?php echo "<?php echo ".$this->generateValueField($this->modelClass,$column,'search')."; ?>\n"; ?>
 <?php endif; ?>
         </div>
+    
 <?php endforeach; ?>
-<?php /*foreach($this->getRelations() as $key => $relation)
-{
-	if($relation[0] == 'CBelongsToRelation'
-			or $relation[0] == 'CHasOneRelation'
-			or $relation[0] == 'CManyManyRelation')
-	{
-		printf('<label for="%s">%s</label>', $relation[1], $relation[1]);
-		echo "<?php echo CHtml::listData({$relation[1]}::model()->findAll(), 'id', 'nombre'". $this->generateRelation($this->modelClass, $key, $relation)."; ?>\n";
-	}
-}*/
-?>
         <div class="row buttons">
                 <?php echo "<?php echo CHtml::submitButton(Yii::t('app', 'Search')); ?>\n"; ?>
         </div>
