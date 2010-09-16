b7c69e9bfb2dc80afecacb0b98a75ecdb8326a4f
schmunk@usrbin.de <schmunk@usrbin.de@fa601203-8fba-3265-82bf-2153ca8df311>
2010-09-16 22:10:08 +0000

    updated demo schema (removed languages)
    git-svn-id: https://gii-template-collection.googlecode.com/svn/trunk/gtc@114 fa601203-8fba-3265-82bf-2153ca8df311

--- _dev/demo_schema.sql
+++ _dev/demo_schema.sql
@@ -2,17 +2,12 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-DROP TABLE IF EXISTS `demo_category`;
-DROP TABLE IF EXISTS `demo_product`;
-DROP TABLE IF EXISTS `demo_feature`;
-DROP TABLE IF EXISTS `demo_language`;
-DROP TABLE IF EXISTS `demo_description`;
-DROP TABLE IF EXISTS `demo_feature_has_product`;
-
 
 -- -----------------------------------------------------
 -- Table `demo_category`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `demo_category` ;
+
 CREATE  TABLE IF NOT EXISTS `demo_category` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NULL ,
@@ -23,10 +18,14 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `demo_product`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `demo_product` ;
+
 CREATE  TABLE IF NOT EXISTS `demo_product` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NULL ,
   `category_id` INT NULL ,
+  `name` VARCHAR(45) NULL ,
+  `madeAt` DATE NULL ,
+  `isAvailable` TINYINT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_product_category1` (`category_id` ASC) ,
   CONSTRAINT `fk_product_category1`
@@ -40,6 +39,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `demo_feature`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `demo_feature` ;
+
 CREATE  TABLE IF NOT EXISTS `demo_feature` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NULL ,
@@ -48,35 +49,19 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `demo_language`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `demo_language` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `code` VARCHAR(45) NULL ,
-  `name` VARCHAR(45) NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `demo_description`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `demo_description` ;
+
 CREATE  TABLE IF NOT EXISTS `demo_description` (
   `title` VARCHAR(45) NULL ,
   `text` TEXT NULL ,
   `product_id` INT NOT NULL ,
-  `language_id` INT NOT NULL ,
   PRIMARY KEY (`product_id`) ,
-  INDEX `fk_description_product1` (`product_id` ASC) ,
-  INDEX `fk_description_language1` (`language_id` ASC) ,
-  CONSTRAINT `fk_description_product1`
+  INDEX `fk_demo_description_demo_product1` (`product_id` ASC) ,
+  CONSTRAINT `fk_demo_description_demo_product1`
     FOREIGN KEY (`product_id` )
     REFERENCES `demo_product` (`id` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_description_language1`
-    FOREIGN KEY (`language_id` )
-    REFERENCES `demo_language` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -85,6 +70,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `demo_feature_has_product`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `demo_feature_has_product` ;
+
 CREATE  TABLE IF NOT EXISTS `demo_feature_has_product` (
   `feature_id` INT NOT NULL ,
   `product_id` INT NOT NULL ,
