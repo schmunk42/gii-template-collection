2843498b58a734e1e2bc7583df424a9601558745
schmunk@usrbin.de <schmunk@usrbin.de@fa601203-8fba-3265-82bf-2153ca8df311>
2010-09-17 08:50:03 +0000

    applied code formatting
    git-svn-id: https://gii-template-collection.googlecode.com/svn/trunk/gtc@115 fa601203-8fba-3265-82bf-2153ca8df311

--- fullCrud/FullCrudCode.php
+++ fullCrud/FullCrudCode.php
@@ -3,6 +3,7 @@
 Yii::import('system.gii.generators.crud.CrudCode');
 
 class FullCrudCode extends CrudCode {
+
 	public $authtype;
 	public $persistent_sessions = true;
 	public $enable_ajax_validation = true;
@@ -12,22 +13,19 @@ class FullCrudCode extends CrudCode {
 		parent::prepare();
 	}
 
-  public function rules()                                                       
-  {
-    return array_merge(parent::rules(), array(
-      array('authtype, persistent_sessions, enable_ajax_validation', 'required'),
-    ));
-  }
-
-  public function attributeLabels()
-  {
-    return array_merge(parent::attributeLabels(), array(
-      'authtype'=>'Authentication type',
-      'persistent_sessions'=>'Persistent Sessions',
-      'enable_ajax_validateion'=>'Enable ajax Validation',
-    ));
+	public function rules() {
+		return array_merge(parent::rules(), array(
+			array('authtype, persistent_sessions, enable_ajax_validation', 'required'),
+		));
 	}
 
+	public function attributeLabels() {
+		return array_merge(parent::attributeLabels(), array(
+			'authtype' => 'Authentication type',
+			'persistent_sessions' => 'Persistent Sessions',
+			'enable_ajax_validateion' => 'Enable ajax Validation',
+		));
+	}
 
 	public function init() {
 		parent::init();
@@ -63,7 +61,7 @@ class FullCrudCode extends CrudCode {
 
 		if (!in_array('GController.php', $names)) {
 			if (!copy($gtcpath . '/GController.php',
-						$controllerspath . '/GController.php'))
+					$controllerspath . '/GController.php'))
 				throw new CException('GController.php could not be copied over to your components directory');
 		}
 	}
@@ -74,10 +72,10 @@ class FullCrudCode extends CrudCode {
 		$j = 0;
 		foreach ($columns as $column) {
 			if (!$column->isForeignKey
-					&& !$column->isPrimaryKey
-					&& $column->type != 'INT'
-					&& $column->type != 'INTEGER'
-					&& $column->type != 'BOOLEAN') {
+				&& !$column->isPrimaryKey
+				&& $column->type != 'INT'
+				&& $column->type != 'INTEGER'
+				&& $column->type != 'BOOLEAN') {
 				$num = $j;
 				break;
 			}
@@ -119,28 +117,27 @@ class FullCrudCode extends CrudCode {
 			$style = $relation[0] == 'CManyManyRelation' ? 'checkbox' : 'dropdownlist';
 
 			if (is_object($field)) {
-				if($relation[0] == 'CManyManyRelation')
-					$allowEmpty='false';
-				elseif($relation[0] == 'CHasOneRelation') {
-					$allowEmpty= (CActiveRecord::model($relation[1])->tableSchema->columns[$relation[2]]->allowNull?'true':'false');
-        				return "echo 'TBD';";
-                                }
-        			else
-                			$allowEmpty= (CActiveRecord::model($model)->tableSchema->columns[$relation[2]]->allowNull?'true':'false');
+				if ($relation[0] == 'CManyManyRelation')
+					$allowEmpty = 'false';
+				elseif ($relation[0] == 'CHasOneRelation') {
+					$allowEmpty = (CActiveRecord::model($relation[1])->tableSchema->columns[$relation[2]]->allowNull ? 'true' : 'false');
+					return "echo 'TBD';";
+				}
+				else
+					$allowEmpty= ( CActiveRecord::model($model)->tableSchema->columns[$relation[2]]->allowNull ? 'true' : 'false');
 
-				return("
-						\$this->widget('ext.Relation', array(
-								'model' => \$model,
-								'relation' => '{$relationname}',
-								'fields' => '{$field->name}',
-								'allowEmpty' => {$allowEmpty},
-								'style' => '{$style}',
-								'htmlOptions' => array(
-									'checkAll' => Yii::t('app', 'Choose all'),
-									),
-
-								)
-							)");
+				return("\$this->widget(
+					'ext.Relation',
+					array(
+					'model' => \$model,
+					'relation' => '{$relationname}',
+					'fields' => '{$field->name}',
+					'allowEmpty' => {$allowEmpty},
+					'style' => '{$style}',
+					'htmlOptions' => array(
+						'checkAll' => Yii::t('app', 'Choose all'),
+					),)
+				)");
 			}
 		}
 	}
@@ -153,18 +150,17 @@ class FullCrudCode extends CrudCode {
 		if (!is_object($model))
 			$model = CActiveRecord::model($model);
 
-		/*if ($column->isForeignKey)
-			return false;*/
+		/* if ($column->isForeignKey)
+		  return false; */
 
 		if (strtoupper($column->dbType) == 'TINYINT(1)'
-				|| strtoupper($column->dbType) == 'BIT'
-				|| strtoupper($column->dbType) == 'BOOL'
-				|| strtoupper($column->dbType) == 'BOOLEAN') {
+			|| strtoupper($column->dbType) == 'BIT'
+			|| strtoupper($column->dbType) == 'BOOL'
+			|| strtoupper($column->dbType) == 'BOOLEAN') {
 			return "echo \$form->checkBox(\$model,'{$column->name}')";
 		} else if (strtoupper($column->dbType) == 'DATE') {
 			$modelname = get_class($model);
-			return ("\$this->widget('zii.widgets.jui.CJuiDatePicker',
-        array(
+			return ("\$this->widget('zii.widgets.jui.CJuiDatePicker',array(
             'model'=>'\$model',
             'name'=>'{$modelname}[{$column->name}]',
             'language'=>Yii::app()->language,
@@ -177,8 +173,7 @@ class FullCrudCode extends CrudCode {
                 'dateFormat'=>'yy-mm-dd',
                 ),
             )
-        );
-");
+			);");
 		} else if (substr(strtoupper($column->dbType), 0, 4) == 'ENUM') {
 			$string = sprintf("echo CHtml::activeDropDownList(\$model, '%s', array(\n", $column->name);
 
@@ -207,14 +202,14 @@ class FullCrudCode extends CrudCode {
 			$table = $model->getTableSchema();
 			$fk = $table->foreignKeys[$column->name];
 
-                        // We have to look into relations to find the correct model class (i.e. if models are generated with table prefix)
-                        // TODO: do not repeat yourself (foreach) - this is a hotfix
-                        foreach ($model->relations() as $key => $value) {
+			// We have to look into relations to find the correct model class (i.e. if models are generated with table prefix)
+			// TODO: do not repeat yourself (foreach) - this is a hotfix
+			foreach ($model->relations() as $key => $value) {
 				if (strcasecmp($value[2], $column->name) == 0)
 					$relation = $value;
 			}
-                        $fmodel = CActiveRecord::model($relation[1]);
-                        $fmodelName = $relation[1];
+			$fmodel = CActiveRecord::model($relation[1]);
+			$fmodelName = $relation[1];
 			#$fmodel = CActiveRecord::model(ucfirst($fk[0]));
 
 			$modelTable = ucfirst($fmodel->tableName());
@@ -228,65 +223,62 @@ class FullCrudCode extends CrudCode {
 			//return("\$model->{$relname}->{$fcolumns[1]}");
 			//return("CHtml::value(\$model,\"{$relname}.{$fcolumns[1]}\")");
 			//return("{$relname}.{$fcolumns[1]}");
-			if ($view===true) {
-				return "
-					array(
-							'name'=>'{$column->name}',
-							'value'=>CHtml::value(\$model,'{$relname}.{$fcolumns[1]}'),
-							)";
-			} elseif($view=='search')
-			return "\$form->dropDownList(\$model,'{$column->name}',CHtml::listData({$fmodelName}::model()->findAll(), '{$fmodel->getTableSchema()->primaryKey}', '{$fcolumns[1]}'),array('prompt'=>Yii::t('app', 'All')))";
+			if ($view === true) {
+				return "array(
+					'name'=>'{$column->name}',
+					'value'=>CHtml::value(\$model,'{$relname}.{$fcolumns[1]}'),
+				)";
+			} elseif ($view == 'search')
+				return "\$form->dropDownList(\$model,'{$column->name}',CHtml::listData({$fmodelName}::model()->findAll(), '{$fmodel->getTableSchema()->primaryKey}', '{$fcolumns[1]}'),array('prompt'=>Yii::t('app', 'All')))";
 			else
-				return "
-					array(
-							'name'=>'{$column->name}',
-							'value'=>'CHtml::value(\$data,\\'{$relname}.{$fcolumns[1]}\\')',
-								'filter'=>CHtml::listData({$fmodelName}::model()->findAll(), '{$fcolumns[0]}', '{$fcolumns[1]}'),
-								)";
-							//{$relname}.{$fcolumns[1]}
-							}
-							elseif (strtoupper($column->dbType) == 'BOOLEAN' or strtoupper($column->dbType) == 'TINYINT(1)' OR strtoupper($column->dbType) == 'BIT') {
+				return "array(
+					'name'=>'{$column->name}',
+					'value'=>'CHtml::value(\$data,\\'{$relname}.{$fcolumns[1]}\\')',
+					'filter'=>CHtml::listData({$fmodelName}::model()->findAll(), '{$fcolumns[0]}', '{$fcolumns[1]}'),
+				)";
+			//{$relname}.{$fcolumns[1]}
+		}
+		elseif (strtoupper($column->dbType) == 'BOOLEAN' or strtoupper($column->dbType) == 'TINYINT(1)' OR strtoupper($column->dbType) == 'BIT') {
 
-							if ($view) {
-							return "
-							array(
-								'name'=>'{$column->name}',
-								'value'=>\$model->{$column->name}?Yii::t('app', 'Yes'):Yii::t('app', 'No'),
-								)";
-							} else
-							return "
-							array(
-								'name'=>'{$column->name}',
-								'value'=>'\$data->{$column->name}?Yii::t(\\'app\\',\\'Yes\\'):Yii::t(\\'app\\', \\'No\\')',
-									'filter'=>array('0'=>Yii::t('app','No'),'1'=>Yii::t('app','Yes')),
-									)";
-								}
-								else {
-								return("'" . $column->name . "'");
-								}
-								}
+			if ($view) {
+				return "array(
+					'name'=>'{$column->name}',
+					'value'=>\$model->{$column->name}?Yii::t('app', 'Yes'):Yii::t('app', 'No'),
+				)";
+			} else
+				return "array(
+					'name'=>'{$column->name}',
+					'value'=>'\$data->{$column->name}?Yii::t(\\'app\\',\\'Yes\\'):Yii::t(\\'app\\', \\'No\\')',
+					'filter'=>array('0'=>Yii::t('app','No'),'1'=>Yii::t('app','Yes')),
+				)";
+		}
+		else {
+			return("'" . $column->name . "'");
+		}
+	}
 
-								public function guessNameColumn($columns) {
-								$name = Yii::t('app', 'name');
+	public function guessNameColumn($columns) {
+		$name = Yii::t('app', 'name');
 
-								foreach ($columns as $column) {
-								if (!strcasecmp($column->name, $name))
-								return $column->name;
-								}
+		foreach ($columns as $column) {
+			if (!strcasecmp($column->name, $name))
+				return $column->name;
+		}
 
-								$title = Yii::t('app', 'title');
+		$title = Yii::t('app', 'title');
 
-								foreach ($columns as $column) {
-									if (!strcasecmp($column->name, $title))
-										return $column->name;
-								}
+		foreach ($columns as $column) {
+			if (!strcasecmp($column->name, $title))
+				return $column->name;
+		}
 
-								foreach ($columns as $column) {
-									if ($column->isPrimaryKey)
-										return $column->name;
-								}
-								return 'id';
-								}
+		foreach ($columns as $column) {
+			if ($column->isPrimaryKey)
+				return $column->name;
+		}
+		return 'id';
+	}
 
 }
+
 ?>
