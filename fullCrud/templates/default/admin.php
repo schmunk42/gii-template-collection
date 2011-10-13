dbdc9699e3066ec6d9e7c3f95a2dd99a3ea8d24a
Tobias Munk <schmunk@usrbin.de>
2011-10-13 20:49:44 +0200

    added TODO

--- fullCrud/templates/default/admin.php
+++ fullCrud/templates/default/admin.php
@@ -39,7 +39,7 @@ echo "<ul>";
 foreach($model->relations() AS $key => $relation){
 	echo  "<li>".
 		Yii::t("app",substr(str_replace("Relation","",$relation[0]),1))." ".
-		CHtml::link(Yii::t("app",$relation[1]), array($this->codeProvider->resolveController($relation).'/admin')).
+		CHtml::link(Yii::t("app",$relation[1]), array($this->codeProvider->resolveController($relation).'/admin')). // TODO: render dynamic links
 		" </li>";
 }
 echo "</ul>";
