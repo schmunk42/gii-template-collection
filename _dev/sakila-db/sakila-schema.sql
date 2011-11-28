6e556974a51b49fa80d23a450ace52ec18f58ec0
Tobias Munk <schmunk@usrbin.de>
2011-11-28 22:26:05 +0100

    PHP naming fixes

--- _dev/sakila-db/sakila-schema.sql
+++ _dev/sakila-db/sakila-schema.sql
@@ -319,7 +319,7 @@ CREATE TABLE store (
 
 CREATE VIEW customer_list
 AS
-SELECT cu.customer_id AS ID, CONCAT(cu.first_name, _utf8' ', cu.last_name) AS name, a.address AS address, a.postal_code AS `zip code`,
+SELECT cu.customer_id AS ID, CONCAT(cu.first_name, _utf8' ', cu.last_name) AS name, a.address AS address, a.postal_code AS `zip_code`,
 	a.phone AS phone, city.city AS city, country.country AS country, IF(cu.active, _utf8'active',_utf8'') AS notes, cu.store_id AS SID
 FROM customer AS cu JOIN address AS a ON cu.address_id = a.address_id JOIN city ON a.city_id = city.city_id
 	JOIN country ON city.country_id = country.country_id;
@@ -358,7 +358,7 @@ GROUP BY film.film_id;
 
 CREATE VIEW staff_list
 AS
-SELECT s.staff_id AS ID, CONCAT(s.first_name, _utf8' ', s.last_name) AS name, a.address AS address, a.postal_code AS `zip code`, a.phone AS phone,
+SELECT s.staff_id AS ID, CONCAT(s.first_name, _utf8' ', s.last_name) AS name, a.address AS address, a.postal_code AS `zip_code`, a.phone AS phone,
 	city.city AS city, country.country AS country, s.store_id AS SID
 FROM staff AS s JOIN address AS a ON s.address_id = a.address_id JOIN city ON a.city_id = city.city_id
 	JOIN country ON city.country_id = country.country_id;
