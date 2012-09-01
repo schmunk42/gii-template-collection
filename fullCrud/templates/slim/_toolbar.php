2683942d7def081ea2470dedf8e4414b1411abfc
Tobias Munk <schmunk@usrbin.de>
2012-09-02 01:23:21 +0200

    cleanup

--- fullCrud/templates/slim/_toolbar.php
+++ fullCrud/templates/slim/_toolbar.php
@@ -1,7 +1,7 @@
 <div class="btn-toolbar">
     <div class="btn-group">
         <?php
-        echo '<?php echo CHtml::link("Manage",array("admin"),array("class"=>"btn btn-inverse")); ?>';
+        echo '<?php echo CHtml::link("Manage",array("admin"),array("class"=>"btn")); ?>';
         echo '<?php
             switch($this->action->id) {
                 case "admin":
@@ -9,18 +9,19 @@
                     break;
                 case "view":
                     echo CHtml::link("Update",array("update","id"=>$model->id),array("class"=>"btn"));
-                    echo CHtml::link("Delete",array("delete"),array("class"=>"btn btn-danger"));
+                    echo CHtml::link("Delete", "#",array("submit"=>array("delete","id"=>$model->id), "class"=>"btn btn-danger","confirm"=>"Do you want to delete this item?"));
                     break;
                 case "update":
                     echo CHtml::link("View",array("view","id"=>$model->id),array("class"=>"btn"));
-                    echo CHtml::link("Delete",array("delete"),array("class"=>"btn btn-danger"));
+                    echo CHtml::link("Delete", "#",array("submit"=>array("delete","id"=>$model->id), "class"=>"btn btn-danger","confirm"=>"Do you want to delete this item?"));
                     break;
             }
         ?>';
         ?>
     </div>
+    <?php echo "<?php if(\$this->action->id == 'admin'): ?>" ?>
     <div class="btn-group">
-        <a class="btn btn-info dropdown-toggle" data-toggle="dropdown" href="#">
+        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
             Relations
             <span class="caret"></span>
         </a>
@@ -42,8 +43,9 @@
         </ul>
     </div>
     <div class="btn-group">
-        <?php echo "<?php if(\$this->action->id == 'admin') echo CHtml::link(Yii::t('app', 'Advanced Search'),'#',array('class'=>'btn search-button')); ?>"; ?>
+        <?php echo "<?php echo CHtml::link(Yii::t('app', 'Advanced Search'),'#',array('class'=>'btn search-button')); ?>"; ?>
     </div>
+    <?php echo "<?php endif; ?>" ?>
 </div>
 
 <div class="search-form" style="display:none">
