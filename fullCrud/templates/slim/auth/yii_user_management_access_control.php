454b43255a7d331546a9522dcaffa0a1743a809d
Tobias Munk <schmunk@usrbin.de>
2013-02-04 02:19:54 +0100

    fixed access filter

--- fullCrud/templates/slim/auth/yii_user_management_access_control.php
+++ fullCrud/templates/slim/auth/yii_user_management_access_control.php
@@ -7,7 +7,7 @@ public function filters() {
 public function accessRules() {
 	return array(
 			array('allow',
-				'actions'=>array('create','ajaxUpdate','update','delete','admin','view'),
+				'actions'=>array('create','editableSaver','update','delete','admin','view'),
 				'roles'=>array('<?php echo $rightsPrefix ?>.*'),
 				),
 			array('deny',
