c420a071aa0c31618ba0df7bb4ccdb0149e6c36d
Tobias Munk <schmunk@usrbin.de>
2013-04-12 16:56:50 +0200

    added bootstrap columns, added bootstrap classes, improved output code formatting

--- fullCrud/templates/slim/_form.php
+++ fullCrud/templates/slim/_form.php
@@ -1,6 +1,6 @@
-<div class="form">
-    <p class="note">
-        <?php echo "<?php echo Yii::t('" . $this->messageCatalog . "','Fields with <span class=\"required\">*</span> are required.');?>"; ?>
+<div class="">
+    <p class="alert">
+        <?php echo "<?php echo Yii::t('" . $this->messageCatalog . "','Fields with <span class=\"required\">*</span> are required.');?> \n"; ?>
     </p>
 
 
@@ -8,7 +8,8 @@
     \$this->widget('echosen.EChosen',
         array('target'=>'select')
     );
-?>"; ?>
+    ?>"; ?>
+
     <?php echo '<?php'; ?>
 
     $form=$this->beginWidget('CActiveForm', array(
@@ -18,7 +19,11 @@
     ));
 
     echo $form->errorSummary($model);
-    ?>
+
+    <?php echo '?>'; ?>
+
+ <div class="row">
+     <div class="span8"> <!-- main inputs -->
 
     <?php
     foreach ($this->tableSchema->columns as $column) {
@@ -33,7 +38,6 @@
             }
         }
 
-
         if (!$column->isForeignKey
             && $column->name != 'create_time'
             && $column->name != 'update_time'
@@ -41,18 +45,20 @@
             && $column->name != 'updatetime'
             && $column->name != 'timestamp'
         ) {
-            echo "<div class=\"row\">\n";
-            echo "<?php " . $this->generateActiveLabel($this->modelClass, $column) . "; ?>\n";
-            echo "<?php " . $this->generateActiveField($this->modelClass, $column) . "; ?>\n";
-            echo "<?php echo \$form->error(\$model,'{$column->name}'); ?>\n";
+            echo "\n";
+            echo "    <div class=\"row-fluid input-block-level-form\">\n";
+            echo "        <div class=\"span12\">\n";
+            echo "            <?php " . $this->generateActiveLabel($this->modelClass, $column) . "; ?>\n";
+            echo "            <?php " . $this->generateActiveField($this->modelClass, $column) . "; ?>\n";
+            echo "            <?php echo \$form->error(\$model,'{$column->name}'); ?>\n";
 
             // renders a hint div, but leaves it empty, when the hint is not translated yet
             $placholder = "help." . $column->name . "";
-            echo "<?php if('" . $placholder . "' != \$help = Yii::t('" . $this->messageCatalog . "', '" . $placholder . "')) { \n";
-            echo '    echo "<span class=\'help-block\'>$help</span>";';
-            echo "\n} ?>";
-
-            echo "</div>\n\n";
+            echo "            <?php if('" . $placholder . "' != \$help = Yii::t('" . $this->messageCatalog . "', '" . $placholder . "')) { \n";
+            echo '                echo "<span class=\'help-block\'>$help</span>";';
+            echo "            \n} ?>\n";
+            echo "        </div>\n";
+            echo "    </div>\n\n";
         }
     }
 
@@ -61,28 +67,41 @@
             || $relation[0] == 'CHasOneRelation'
             || $relation[0] == 'CManyManyRelation'
         ) {
-            echo "<div class=\"row\">\n";
-            printf("<label for=\"%s\"><?php echo Yii::t('" . $this->messageCatalog . "', '%s'); ?></label>\n", $key, ucfirst($key));
-            echo "<?php " . $this->codeProvider->generateRelation($this->modelClass, $key, $relation) . "; ?><br />\n";
-            echo "</div>\n\n";
+            echo "    <div class=\"row-fluid input-block-level-form\">\n";
+            echo "        <div class=\"span12\">\n";
+            printf("        <label for=\"%s\"><?php echo Yii::t('" . $this->messageCatalog . "', '%s'); ?></label>\n", $key, ucfirst($key));
+            echo "                <?php\n";
+            echo "                ".$this->codeProvider->generateRelation($this->modelClass, $key, $relation);
+            echo "\n              ?>\n";
+            echo "        </div>\n";
+            echo "    </div>\n\n";
         }
     }
     ?>
+    </div> <!-- main inputs -->
+
+
+    <div class="span4"> <!-- sub inputs -->
+
+    </div> <!-- sub inputs -->
+</div>
+
 
     <div class="form-actions">
         <?php
         echo "
     <?php
-echo CHtml::Button(Yii::t('" . $this->messageCatalog . "', 'Cancel'), array(
+        echo CHtml::Button(Yii::t('" . $this->messageCatalog . "', 'Cancel'), array(
 			'submit' => (isset(\$_GET['returnUrl']))?\$_GET['returnUrl']:array('" . strtolower($this->modelClass) . "/admin'),
 			'class' => 'btn'
 			));
-echo ' '.CHtml::submitButton(Yii::t('" . $this->messageCatalog . "', 'Save'), array(
+        echo ' '.CHtml::submitButton(Yii::t('" . $this->messageCatalog . "', 'Save'), array(
             'class' => 'btn btn-primary'
-)); ?>\n";
+            ));
+    ?>\n";
         ?>
-    </div>
+</div>
 
-    <?php echo "<?php \$this->endWidget() ?>"; ?>
+<?php echo "<?php \$this->endWidget() ?>"; ?>
 
 </div> <!-- form -->
