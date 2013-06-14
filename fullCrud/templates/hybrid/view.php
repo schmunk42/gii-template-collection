672a2f371af966803a8d76b846578fd184088a6a
Fredrik Wollsén <fredrik@neam.se>
2013-06-15 01:35:26 +0200

    Added simple properties and relations listing from default template to view

--- fullCrud/templates/hybrid/view.php
+++ fullCrud/templates/hybrid/view.php
@@ -20,6 +20,91 @@ echo "?>";
 
 <?php echo '<?php $this->renderPartial("_toolbar", array("model"=>$model)); ?>'; ?>
 
+<?php
+echo "\t<b><?php echo CHtml::encode(\$model->getAttributeLabel('{$this->identificationColumn}')); ?>:</b>\n";
+echo "\t<?php echo CHtml::link(CHtml::encode(\$model->{$this->identificationColumn}), array('view', '{$this->identificationColumn}'=>\$model->{$this->identificationColumn})); ?>\n\t<br />\n\n";
+$count = 0;
+foreach ($this->tableSchema->columns as $column) {
+    if ($column->isPrimaryKey)
+	continue;
+    if (++$count == 7)
+	echo "\t<?php /*\n";
+    echo "\t<b><?php echo CHtml::encode(\$model->getAttributeLabel('{$column->name}')); ?>:</b>\n";
+    if ($column->name == 'createtime'
+	or $column->name == 'updatetime'
+	or $column->name == 'timestamp') {
+	echo "\techo Yii::app()->getDateFormatter()->formatDateTime(\$model->{$column->name}, 'medium', 'medium'); ?>\n\t<br />\n\n";
+    } else {
+	echo "\t<?php echo CHtml::encode(\$model->{$column->name}); ?>\n\t<br />\n\n";
+    }
+}
+if ($count >= 7)
+    echo "\t*/ ?>\n";
+?>
+
+<?php
+foreach (CActiveRecord::model(Yii::import($this->model))->relations() as $key => $relation) {
+
+    $controller = $this->codeProvider->resolveController($relation);
+    $relatedModel = CActiveRecord::model($relation[1]);
+    $pk = $relatedModel->tableSchema->primaryKey;
+    $suggestedfield = $this->suggestName($relatedModel->tableSchema->columns);
+
+    // TODO: currently composite PKs are omitted
+    if (is_array($pk))
+        continue;
+
+    if (($relation[0] == 'CManyManyRelation' || $relation[0] == 'CHasManyRelation')) {
+        #$model = CActiveRecord::model($relation[1]);
+        #if (!$pk = $model->tableSchema->primaryKey)
+        #	$pk = 'id';
+        #$suggestedtitle = $this->suggestName($model->tableSchema->columns);
+        echo '<h2>';
+        echo "<?php echo CHtml::link(Yii::t('app','" . ucfirst($key) . "'), array('" . $controller . "/admin'));?>";
+        echo "</h2>\n";
+        echo CHtml::openTag('ul');
+        echo "
+				<?php if (is_array(\$model->{$key})) foreach(\$model->{$key} as \$foreignobj) { \n
+					echo '<li>';
+					echo CHtml::link(\$foreignobj->$suggestedfield, array('{$controller}/view','{$pk}'=>\$foreignobj->{$pk}));\n
+						echo ' '.CHtml::link(Yii::t('app','Update'), array('{$controller}/update','{$pk}'=>\$foreignobj->{$pk}), array('class'=>'edit'));\n
+				}
+			?>";
+        echo CHtml::closeTag('ul');
+
+        echo "<p><?php echo CHtml::link(
+				Yii::t('app','Create'),
+				array('" . $controller . "/create', '$relation[1]' => array('$relation[2]'=>\$model->{\$model->tableSchema->primaryKey}))
+					);  ?></p>";
+    }
+    if ($relation[0] == 'CHasOneRelation') {
+        $relatedModel = CActiveRecord::model($relation[1]);
+        if (!$pk = $relatedModel->tableSchema->primaryKey)
+            $pk = 'id';
+
+#$suggestedtitle = $this->suggestName($model->tableSchema->columns);
+        echo '<h2>';
+        echo "<?php echo CHtml::link(Yii::t('app','" . $relation[1] . "'), array('" . $controller . "/admin'));?>";
+        echo "</h2>\n";
+        echo CHtml::openTag('ul');
+        echo "<?php \$foreignobj = \$model->{$key}; \n
+				if (\$foreignobj !== null) {
+					echo '<li>';
+					echo '#'.\$model->{$key}->{$pk}.' ';
+					echo CHtml::link(\$model->{$key}->$suggestedfield, array('{$controller}/view','{$pk}'=>\$model->{$key}->{$pk}));\n
+						echo ' '.CHtml::link(Yii::t('app','Update'), array('{$controller}/update','{$pk}'=>\$model->{$key}->{$pk}), array('class'=>'edit'));\n
+
+
+				}
+			?>";
+        echo CHtml::closeTag('ul');
+        echo "<p><?php if(\$model->{$key} === null) echo CHtml::link(
+				Yii::t('app','Create'),
+				array('" . $controller . "/create', '$relation[1]' => array('$relation[2]'=>\$model->{\$model->tableSchema->primaryKey}))
+					);  ?></p>";
+    }
+}
+?>
 
 <h2>
     <?php echo "<?php echo Yii::t('".$this->messageCatalog."','Data')?>";?>
