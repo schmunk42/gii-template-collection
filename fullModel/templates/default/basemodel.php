1a274e6643f11f037486bce1dd87994cecbe929a
Fredrik Wollsén <fredrik@neam.se>
2013-06-16 18:55:53 +0200

    Added getRelatedSearchModel method to basemodel

--- fullModel/templates/default/basemodel.php
+++ fullModel/templates/default/basemodel.php
@@ -137,4 +137,40 @@ abstract class <?php echo 'Base' . $modelClass; ?> extends <?php echo $this->bas
 		));
 	}
 
+	/**
+	 * Returns a model used to populate a filterable, searchable
+	 * and sortable CGridView with the records found by a model relation.
+	 *
+	 * Usage:
+	 * $relatedSearchModel = $model->getRelatedSearchModel('relationName');
+	 *
+	 * Then, when invoking CGridView:
+	 * 	...
+	 * 		'dataProvider' => $relatedSearchModel->search(),
+	 * 		'filter' => $relatedSearchModel,
+	 * 	...
+	 * @returns CActiveRecord
+	 */
+	public function getRelatedSearchModel($name)
+	{
+
+		$md = $this->getMetaData();
+		if (!isset($md->relations[$name]))
+			throw new CDbException(Yii::t('yii', '{class} does not have relation "{name}".', array('{class}' => get_class($this), '{name}' => $name)));
+
+		$relation = $md->relations[$name];
+		if (!($relation instanceof CHasManyRelation))
+			throw new CException("Currently works with HAS_MANY relations only");
+
+		$className = $relation->className;
+		$related = new $className('search');
+		$related->unsetAttributes();
+		$related->{$relation->foreignKey} = $this->primaryKey;
+		if (isset($_GET[$className]))
+		{
+			$related->attributes = $_GET[$className];
+		}
+		return $related;
+	}
+
 }
