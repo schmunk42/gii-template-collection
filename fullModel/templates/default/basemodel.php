0bc19d25cf2fb131caee594bdd76bec786d66d3a
Fredrik Wollsén <fredrik@neam.se>
2013-07-10 16:14:47 +0200

    Whitespace fix

--- fullModel/templates/default/basemodel.php
+++ fullModel/templates/default/basemodel.php
@@ -117,7 +117,7 @@ abstract class <?php echo 'Base' . $modelClass; ?> extends <?php echo $this->bas
 	public function search($criteria = null)
 	{
         if (is_null($criteria)) {
-		$criteria=new CDbCriteria;
+    		$criteria=new CDbCriteria;
         }
 
 <?php
