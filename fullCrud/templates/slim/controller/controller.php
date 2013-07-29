e43ff00a9d3d8421d62bbdcd30f1251710a47c24
Fredrik Wollsén <fredrik@neam.se>
2013-07-29 10:15:54 +0300

    Generated code related to many-many relations now formatted according to PSR-2

--- fullCrud/templates/slim/controller/controller.php
+++ fullCrud/templates/slim/controller/controller.php
@@ -59,8 +59,9 @@ class <?php echo $this->controllerClass; ?> extends <?php echo $this->baseContro
             {
                 if($relation[0] == 'CManyManyRelation')
                 {
-                    printf("            if(isset(\$_POST['%s']['%s']))\n", $this->modelClass, $relation[1]);
+                    printf("            if (isset(\$_POST['%s']['%s'])) {\n", $this->modelClass, $relation[1]);
                     printf("                \$model->setRelationRecords('%s', \$_POST['%s']['%s']);\n", $key, $this->modelClass, $relation[1]);
+                    print("            }\n");
                 }
             }
 ?>
@@ -98,10 +99,11 @@ class <?php echo $this->controllerClass; ?> extends <?php echo $this->baseContro
             {
                 if($relation[0] == 'CManyManyRelation')
                 {
-                    printf("            if(isset(\$_POST['%s']['%s']))\n", $this->modelClass, $relation[1]);
+                    printf("            if (isset(\$_POST['%s']['%s'])) {\n", $this->modelClass, $relation[1]);
                     printf("                \$model->setRelationRecords('%s', \$_POST['%s']['%s']);\n", $key, $this->modelClass, $relation[1]);
-                    echo "else\n";
-                    echo "\$model->setRelationRecords('{$key}',array());\n";
+                    print("            } else {\n");
+                    print("                \$model->setRelationRecords('{$key}', array());\n");
+                    print("            }\n");
                 }
             }
 ?>
