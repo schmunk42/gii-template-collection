d0afe76b56a33886e6d131b1b2d9b7c3c5441e62
Tobias Munk <schmunk@usrbin.de>
2013-07-31 00:28:12 +0200

    changed to bobsled

--- fullCrud/templates/slim/update.php
+++ fullCrud/templates/slim/update.php
@@ -1,24 +1,35 @@
 <?php
 $label = $this->pluralize($this->class2name($this->modelClass));
-echo "<?php\n";
-echo "\$this->breadcrumbs[Yii::t('".$this->messageCatalog."','$label')] = array('admin');\n";
-echo "\$this->breadcrumbs[\$model->{\$model->tableSchema->primaryKey}] = array('view','id'=>\$model->{\$model->tableSchema->primaryKey});\n";
-echo "\$this->breadcrumbs[] = Yii::t('".$this->messageCatalog."', 'Update');\n";
-echo "?>";
 ?>
 
-<?php echo '<?php $this->widget("TbBreadcrumbs", array("links"=>$this->breadcrumbs)) ?>'; ?>
+<?=
+"<?php
+\$this->breadcrumbs[Yii::t('{$this->messageCatalog}','{$label}')] = array('admin');
+\$this->breadcrumbs[\$model->{\$model->tableSchema->primaryKey}] = array('view','id'=>\$model->{\$model->tableSchema->primaryKey});
+\$this->breadcrumbs[] = Yii::t('{$this->messageCatalog}', 'Update');
+?>
+";?>
 
-<h1>
-    <?php
-    echo "<?php echo Yii::t('".$this->messageCatalog."','".$this->class2name($this->modelClass)."')?>";
-    echo " <small><?php echo Yii::t('".$this->messageCatalog."','Update')?> #<?php echo \$model->" . $this->tableSchema->primaryKey . " ?></small>";
-    ?>
-</h1>
+<?= '<?php $this->widget("TbBreadcrumbs", array("links"=>$this->breadcrumbs)) ?>'; ?>
 
-<?php echo '<?php $this->renderPartial("_toolbar", array("model"=>$model)); ?>'; ?>
+    <h1>
+        <?=
+        "
+        <?php echo Yii::t('{$this->messageCatalog}','{$this->class2name($this->modelClass)}');
+        <small>
+            <?php echo Yii::t('{$this->messageCatalog}','Update')?> #<?php echo \$model->{$this->tableSchema->primaryKey} ?>
+        </small>
+        ";
+        ?>
 
-<?php echo "<?php\n"; ?>
-$this->renderPartial('_form', array(
-'model'=>$model));
+    </h1>
+
+<?= '<?php $this->renderPartial("_toolbar", array("model"=>$model)); ?>'; ?>
+
+<?=
+"
+<?php
+    \$this->renderPartial('_form', array('model'=>\$model));
+?>
+"
 ?>
