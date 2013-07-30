e41575ffd3cea0769c0a453bff262ecf18ad8ab9
Tobias Munk <schmunk@usrbin.de>
2013-07-31 00:11:25 +0200

    updated search template (using from common folder)

--- fullCrud/templates/hybrid/_search.php
+++ fullCrud/templates/hybrid/_search.php
@@ -1,30 +1,3 @@
-<div class="wide form">
+<?php
 
-    <?php echo "<?php \$form=\$this->beginWidget('CActiveForm', array(
-        'action'=>Yii::app()->createUrl(\$this->route),
-        'method'=>'get',
-)); ?>\n"; ?>
-
-    <?php foreach ($this->tableSchema->columns as $column): ?>
-        <?php
-        $field = $this->provider()->generateInputField($this->modelClass, $column);
-        if (strpos($field, 'password') !== false)
-            continue;
-        ?>
-        <div class="row">
-            <?php echo "<?php echo \$form->label(\$model,'{$column->name}'); ?>\n"; ?>
-            <?php if (!$column->isForeignKey): ?>
-                <?php echo "<?php " . $this->provider()->generateActiveField($this->modelClass, $column) . "; ?>\n"; ?>
-            <?php else: ?>
-                <?php echo "<?php " . $this->provider()->generateValueField($this->modelClass, $column, 'search') . "; ?>\n"; ?> // TODO: itemLabel
-            <?php endif; ?>
-        </div>
-
-    <?php endforeach; ?>
-    <div class="row buttons">
-        <?php echo "<?php echo CHtml::submitButton(Yii::t('".$this->messageCatalog."', 'Search')); ?>\n"; ?>
-    </div>
-
-    <?php echo "<?php \$this->endWidget(); ?>\n"; ?>
-
-</div><!-- search-form -->
+require(dirname(__FILE__)."/../_common/_search.php");
--- fullCrud/templates/slim/_search.php
+++ fullCrud/templates/slim/_search.php
@@ -1,35 +1,3 @@
-<div class="wide form">
+<?php
 
-    <?=
-    "<?php \$form=\$this->beginWidget('CActiveForm', array(
-           'action'=>Yii::app()->createUrl(\$this->route),
-           'method'=>'get',
-   )); ?>\n"; ?>
-
-    <?php foreach ($this->tableSchema->columns as $column): ?>
-        <?php
-        $field = $this->provider()->generateInputField($this->modelClass, $column);
-        if (strpos($field, 'password') !== false) {
-            continue;
-        }
-        ?>
-
-        <div class="row">
-            <?= "<?php echo \$form->label(\$model,'{$column->name}'); ?>"; ?>
-            <?= "\n"; ?>
-            <?php if (!$column->isForeignKey): ?>
-                <?= "<?php " . $this->provider()->generateActiveField($this->modelClass, $column) . "; ?>"; ?>
-            <?php else: ?>
-                <?= "<?php " . $this->provider()->generateValueField($this->modelClass, $column, 'search') . "; ?>"; // TODO: itemLabel ?>
-            <?php endif; ?>
-
-        </div>
-
-    <?php endforeach; ?>
-    <div class="row buttons">
-        <?= "<?php echo CHtml::submitButton(Yii::t('" . $this->messageCatalog . "', 'Search')); ?>\n"; ?>
-    </div>
-
-    <?php echo "<?php \$this->endWidget(); ?>\n"; ?>
-
-</div><!-- search-form -->
+require(dirname(__FILE__)."/../_common/_search.php");
