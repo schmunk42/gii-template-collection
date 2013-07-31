d67f4c4f49dd7b03de7675e37de51adb8a2f6b06
Tobias Munk <schmunk@usrbin.de>
2013-07-31 16:26:17 +0200

    updated to bobsled

--- fullCrud/templates/slim/view.php
+++ fullCrud/templates/slim/view.php
@@ -1,59 +1,60 @@
-<?php
-$label = $this->pluralize($this->class2name($this->modelClass));
-
-echo "<?php\n";
-echo "\$this->breadcrumbs[Yii::t('" . $this->messageCatalog . "','$label')] = array('admin');\n";
-echo "\$this->breadcrumbs[] = \$model->{$this->tableSchema->primaryKey};\n";
-echo "?>";
+<?=
+"<?php
+\$this->breadcrumbs[Yii::t('{$this->messageCatalog}','{$this->pluralize($this->class2name($this->modelClass))}')] = array('admin');
+\$this->breadcrumbs[\$model->{\$model->tableSchema->primaryKey}] = array('view','id'=>\$model->{\$model->tableSchema->primaryKey});
+\$this->breadcrumbs[] = Yii::t('{$this->messageCatalog}', 'View');
 ?>
+";?>
 
-<?php echo '<?php $this->widget("TbBreadcrumbs", array("links"=>$this->breadcrumbs)) ?>'; ?>
+<?= '<?php $this->widget("TbBreadcrumbs", array("links"=>$this->breadcrumbs)) ?>'; ?>
 
 <h1>
-    <?php
-    echo "<?php echo Yii::t('" . $this->messageCatalog . "','" . $this->class2name($this->modelClass) . "')?>";
-    echo " <small><?php echo Yii::t('" . $this->messageCatalog . "','View')?> #<?php echo \$model->" . $this->tableSchema->primaryKey . " ?></small>";
-    ?>
+    <?=
+    "<?php echo Yii::t('" . $this->messageCatalog . "','" . $this->class2name($this->modelClass) . "')?>
+    <small><?php echo Yii::t('" . $this->messageCatalog . "','View')?> #<?php echo \$model->" . $this->tableSchema->primaryKey . " ?></small>
+    "?>
 </h1>
 
 
 
-<?php echo '<?php $this->renderPartial("_toolbar", array("model"=>$model)); ?>'; ?>
+<?= '<?php $this->renderPartial("_toolbar", array("model"=>$model)); ?>'; ?>
 
 
 
 <div class="row">
     <div class="span8">
         <h2>
-            <?php echo "<?php echo Yii::t('" . $this->messageCatalog . "','Data')?>"; ?>
+            <?= "<?php echo Yii::t('" . $this->messageCatalog . "','Data')?>"; ?>
         </h2>
 
         <h3>
-            <?php echo "<?php echo \$model->" . $this->provider()->suggestIdentifier(
-                    CActiveRecord::model(Yii::import($this->model))
-                ) . "?>"; ?>
+            <?=
+            "<?php echo \$model->" . $this->provider()->suggestIdentifier(
+                CActiveRecord::model(Yii::import($this->model))
+            ) . "?>"; ?>
         </h3>
 
 
-        <?php
-        echo "<?php
-    \$this->widget('TbDetailView', array(
-    'data'=>\$model,
-    'attributes'=>array(
-    ";
+        <?=
+        "<?php
+        \$this->widget(
+            'TbDetailView',
+            array(
+                'data'=>\$model,
+                'attributes'=>array(
+        ";
         foreach ($this->tableSchema->columns as $column) {
             echo $this->provider()->generateAttribute($column);
         }
         echo "),
-        )); ?>";
-        ?>
+            ));
+        ?>"?>
 
     </div>
 
     <div class="span4">
-        <?php echo "
-        <?php \$this->renderPartial('_view-relations',array('model'=>\$model)); ?>
-        ";
+        <?=
+        "<?php \$this->renderPartial('_view-relations',array('model'=>\$model)); ?>";
         ?>
     </div>
 </div>
