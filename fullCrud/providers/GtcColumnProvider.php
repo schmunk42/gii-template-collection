6d26be83092052b233fe975ef3e93fc2f7bdd06e
Tobias Munk <schmunk@usrbin.de>
2013-08-21 04:50:34 +0200

    cleanup

--- fullCrud/providers/GtcColumnProvider.php
+++ fullCrud/providers/GtcColumnProvider.php
@@ -16,9 +16,14 @@ class GtcColumnProvider extends GtcCodeProvider
             $fk                = $table->foreignKeys[$column->name];
 
             // We have to look into relations to find the correct model class (i.e. if models are generated with table prefix)
-            // TODO: do not repeat yourself (foreach) - this is a hotfix
             foreach ($model->relations() as $key => $value) {
+                // omit relations with array definition
+                if (is_array($value[2])) {
+                    continue;
+                }
+
                 if (strcasecmp($value[2], $column->name) == 0) {
+                    $relname  = $key;
                     $relation = $value;
                 }
             }
@@ -31,12 +36,6 @@ class GtcColumnProvider extends GtcCodeProvider
             $relatedModelName = $relation[1];
             $fcolumns         = $relatedModel->attributeNames();
 
-            $relname = strtolower($fk[0]);
-            foreach ($model->relations() as $key => $value) {
-                if (strcasecmp($value[2], $column->name) == 0) {
-                    $relname = $key;
-                }
-            }
             if ($view === true) {
                 return "array(
                     'name'=>'{$column->name}',
@@ -52,26 +51,23 @@ class GtcColumnProvider extends GtcCodeProvider
                             'filter'=>CHtml::listData({$relatedModelName}::model()->findAll(array('limit'=>1000)), '{$fcolumns[0]}', '{$suggestIdentifier}'),
                             )";
             }
-        } else {
-            /*if (strtoupper($column->dbType) == 'BOOLEAN'
-                or strtoupper($column->dbType) == 'TINYINT(1)' or
-                strtoupper($column->dbType) == 'BIT'
-            ) {
-                if ($view) {
-                    return "array(
+        } elseif (strtoupper($column->dbType) == 'BOOLEAN'
+            or strtoupper($column->dbType) == 'TINYINT(1)' or
+            strtoupper($column->dbType) == 'BIT'
+        ) {
+            if ($view) {
+                return "array(
                     'name'=>'{$column->name}',
-                    'value'=>\$model->{$column->name}?'yes':'no',
-                    )";
-                }
-                else {
-                    return "array(
+                    'value'=>\$model->{$column->name}?\'yes\':\'no\',
+                )";
+            } else {
+                return "array(
                     'name'=>'{$column->name}',
-                    'value'=>'\$data->{$column->name}?'yes':'no',
-                            'filter'=>array('0'=>'no','1'=>'yes'),
-                            )";
-                }
+                    'value'=>'\$data->{$column->name}?\'yes\':\'no\'',
+                    'filter'=>array('0'=>'no','1'=>'yes'),
+                )";
             }
-            else {*/
+        } else { // TODO: useful?, needed?
             if ($column->name == 'createtime'
                 or $column->name == 'updatetime'
                 or $column->name == 'timestamp'
