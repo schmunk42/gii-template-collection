485a6dc0fd63c35e99117544ea0a50a5c8c8163c
Fredrik Wollsén <fredrik@neam.se>
2013-09-06 14:12:57 -0700

    Updated hybrid deps

--- fullCrud/views/hybrid_template_deps.php
+++ fullCrud/views/hybrid_template_deps.php
@@ -5,28 +5,9 @@
         composer.json: </p>
 
     <pre>
-    "repositories":[
-        {
-            "type": "package",
-            "package": {
-                "name": "clevertech/yiibooster",
-                "version": "dev-master",
-                "source": {
-                        "url": "https://github.com/motin/YiiBooster.git",
-                    "type": "git",
-                "reference": "ed6453edd0dc92b5044ee923d71c9e0742b8723f"
-                }
-            }
-        },
-        {
-            "type":"composer",
-            "url":"http://packages.phundament.com"
-        },
-        ...
-    ],
     "require":{
         "php":">=5.3.2",
-        "yiisoft/yii":"1.1.13",
+        "yiisoft/yii":">=1.1.13",
         "vitalets/x-editable-yii": "1.2.0",
         "clevertech/YiiBooster": "dev-master",
         ...
