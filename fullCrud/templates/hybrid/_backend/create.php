a8176f721e42cb9bef67bb0aeb7d0ea1d54df1dd
Fredrik Wollsén <fredrik@neam.se>
2013-09-24 12:25:19 +0300

    Linked create template file from common

--- fullCrud/templates/hybrid/_backend/create.php
+++ fullCrud/templates/hybrid/_backend/create.php
@@ -1,26 +1,3 @@
 <?php
-echo "<?php\n";
-$label = $this->pluralize($this->class2name($this->modelClass));
-echo "\$this->breadcrumbs[Yii::t('".$this->messageCatalog."','$label')] = array('admin');\n";
-echo "\$this->breadcrumbs[] = Yii::t('".$this->messageCatalog."', 'Create');\n";
-echo "?>";
-?>
 
-<?php echo '<?php $this->widget("TbBreadcrumbs", array("links"=>$this->breadcrumbs)) ?>'; ?>
-
-<h1>
-    <?php
-    echo "<?php echo Yii::t('".$this->messageCatalog."','".$this->class2name($this->modelClass)."')?>";
-    echo " <small><?php echo Yii::t('".$this->messageCatalog."','Create')?></small>";
-    ?>
-</h1>
-
-<?php echo '<?php $this->renderPartial("_toolbar", array("model"=>$model)); ?>'; ?>
-
-<?php echo "<?php\n"; ?>
-$this->renderPartial('_form', array(
-'model' => $model,
-'buttons' => 'create'));
-
-?>
-
+require(dirname(__FILE__) . "/../../_common/create.php");
