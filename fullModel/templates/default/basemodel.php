157daeb551d7dad81d404dd072cd3cd77c5eca32
Fredrik Wollsén <fredrik@neam.se>
2013-10-24 14:53:03 +0200

    Refactored search() as per discussion - searchCriteria() with generated search criteria in base model, search() in model

--- fullModel/templates/default/basemodel.php
+++ fullModel/templates/default/basemodel.php
@@ -135,4 +135,29 @@ abstract class <?php echo 'Base' . $modelClass; ?> extends <?php echo $this->bas
         );
     }
 
+    public function searchCriteria($criteria = null)
+    {
+        if (is_null($criteria)) {
+            $criteria = new CDbCriteria;
+        }
+
+    <?php
+    foreach($columns as $name=>$column)
+    {
+        if($column->type==='string' and !$column->isForeignKey)
+        {
+            echo "        \$criteria->compare('t.$name', \$this->$name, true);\n";
+        }
+        else
+        {
+            echo "        \$criteria->compare('t.$name', \$this->$name);\n";
+        }
+    }
+    echo "\n";
+    ?>
+
+        return $criteria;
+
+    }
+
 }
