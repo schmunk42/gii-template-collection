7362debf05e85fb6caa87109ea3daa2d378564e6
Fredrik Wollsén <fredrik@neam.se>
2014-01-15 21:12:50 +0100

    Model search methods take existing optional criteria parameter

--- fullModel/templates/default/model.php
+++ fullModel/templates/default/model.php
@@ -59,10 +59,13 @@ class <?php echo $modelClass; ?> extends <?php echo 'Base' . $modelClass."\n"; ?
         );
     }
 
-    public function search()
+    public function search($criteria = null)
     {
+        if (is_null($criteria)) {
+            $criteria = new CDbCriteria;
+        }
         return new CActiveDataProvider(get_class($this), array(
-            'criteria' => $this->searchCriteria(),
+            'criteria' => $this->searchCriteria($criteria),
         ));
     }
 
