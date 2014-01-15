06be6b4b3b16beb544d7e303363ed37afdbcddc9
Fredrik Wollsén <fredrik@neam.se>
2014-01-15 21:12:24 +0100

    Bug fix: yiiboosters editable lib for hybrid theme controllers

--- fullCrud/templates/hybrid/controller.php
+++ fullCrud/templates/hybrid/controller.php
@@ -123,8 +123,7 @@ class <?php echo $this->controllerClass; ?> extends <?php echo $this->baseContro
 
     public function actionEditableSaver()
     {
-        Yii::import('EditableSaver'); //or you can add import 'ext.editable.*' to config
-        $es = new EditableSaver('<?php echo $this->modelClass; ?>'); // classname of model to be updated
+        $es = new TbEditableSaver('<?php echo $this->modelClass; ?>'); // classname of model to be updated
         $es->update();
     }
 
