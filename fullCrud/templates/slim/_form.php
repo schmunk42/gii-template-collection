ab3cdef5bf56548a5e138c1d65df87dd2d6399b6
Christopher Stebe <chris@stebe.eu>
2014-03-27 18:36:11 +0100

    fix id instead of class for js selector

--- fullCrud/templates/slim/_form.php
+++ fullCrud/templates/slim/_form.php
@@ -4,7 +4,7 @@
     "
     <?php
         Yii::app()->bootstrap->registerPackage('select2');
-        Yii::app()->clientScript->registerScript('crud/variant/update','$(\".{$this->class2id($this->modelClass)}-form select\").select2();');
+        Yii::app()->clientScript->registerScript('crud/variant/update','$(\"#{$this->class2id($this->modelClass)}-form select\").select2();');
 
 
         \$form=\$this->beginWidget('TbActiveForm', array(
